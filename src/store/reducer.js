6af360b6a94ae7eb43ab47954222082fa05f4d87
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-12-01 14:00:04 +0100

    Revert "Dispatching Actions from within the Component. closes #3"

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,24 +3,6 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return {
-                counter: state.counter + 1
-            }
-        case 'DECREMENT':
-            return {
-                counter: state.counter - 1
-            }
-        case 'ADD':
-            return {
-                counter: state.counter + 5
-            }
-        case 'SUBTRACT':
-            return {
-                counter: state.counter - 5
-            }
-    }
     return state;
 }
 
